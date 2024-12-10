4dfc393b2e7f6e8fb52755511d7bd7492314489a
unknown <alejandroalbbercabarcelona@gmail.com>
2024-12-10 23:57:01 +0100

    Deploy to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
-const isProduction = process.env.NODE_ENV === 'production'
-const prefix = isProduction ? '/pagina-ansiedad' : ''
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  assetPrefix: prefix,
-  basePath: prefix,
+  assetPrefix: '/pagina-ansiedad/',
+  basePath: '/pagina-ansiedad',
 }
 
 module.exports = nextConfig
